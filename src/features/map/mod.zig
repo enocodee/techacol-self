17efab7965a1d12fd555d020b96b08684c0994b2
LeviathanST <phamquochung170505@gmail.com>
2026-02-12 20:29:56 +0700

    add `colision detection` between player and map

--- src/features/map/mod.zig
+++ src/features/map/mod.zig
@@ -17,11 +17,46 @@ pub fn build(w: *World) void {
     );
 }
 
+/// 1 is wall, 0 is ground
+// TODO: remove
+pub const collision_block_arr =
+    &[_]u2{1} ** 120 ** 2 ++
+    (&[_]u2{1} ++ &[_]u2{0} ** 118 ++ &[_]u2{1}) ** 56 ++
+    &[_]u2{1} ** 120;
+
 fn spawn(w: *World) !void {
     const map_img = try common.raylib.loadImage("assets/map.png");
-    _ = w.spawnEntity(&.{
+
+    try w.spawnEntity(&.{
         Map{},
         try common.Texture2D.fromImage(map_img),
         Transform.fromXYZ(0, 0, 0),
-    });
+    }).withChildren(struct {
+        pub fn cb(parent: eno.ecs.Entity) !void {
+            const parent_transform = (try parent.getComponents(&.{Transform}))[0];
+            var grid = common.Grid{
+                .num_of_cols = 120,
+                .num_of_rows = 59,
+                .cell_gap = 0,
+                .cell_height = 16,
+                .cell_width = 16,
+                .color = .red,
+                .render_mode = .none,
+            };
+            grid.initCells(parent.world.alloc, parent_transform.x, parent_transform.y);
+
+            const entity = parent.spawn(&.{
+                common.GridBundle{
+                    .grid = grid,
+                    .transform = Transform.fromXYZ(
+                        parent_transform.x,
+                        parent_transform.y,
+                        1,
+                    ),
+                },
+            });
+
+            _ = parent.setComponent(common.InGrid, .{ .grid_entity = entity.id });
+        }
+    }.cb);
 }
--- src/features/player/mod.zig
+++ src/features/player/mod.zig
@@ -1,5 +1,4 @@
-const std = @import("std");
-pub const eno = @import("eno");
+const eno = @import("eno");
 const common = eno.common;
 const rl = common.raylib;
 const scheds = common.schedules;
@@ -9,6 +8,9 @@ const Query = eno.ecs.query.Query;
 const With = eno.ecs.query.With;
 const Transform = common.Transform;
 
+const Map = @import("../map/mod.zig").Map;
+const collision_block_arr = @import("../map/mod.zig").collision_block_arr;
+
 const Player = struct {};
 
 const VELOCITY = 5;
@@ -16,7 +18,15 @@ const VELOCITY = 5;
 pub fn build(w: *World) void {
     _ = w
         .addSystem(.system, scheds.startup, spawn)
-        .addSystems(.system, scheds.update, &.{ movement, updateCam, onWindowResize });
+        .addSystems(
+        .system,
+        scheds.update,
+        &.{
+            movement,
+            updateCam,
+            onWindowResize,
+        },
+    );
 }
 
 fn spawn(w: *World) !void {
@@ -54,17 +64,106 @@ fn updateCam(
 }
 
 fn movement(
-    player_q: Query(&.{
-        *Transform,
-        With(&.{Player}),
-    }),
+    w: *World,
+    player_q: Query(&.{ *Transform, rl.Texture2D, With(&.{Player}) }),
+    map_q: Query(&.{ common.InGrid, With(&.{Map}) }),
 ) !void {
-    const transform = player_q.single()[0];
+    const player_transform: *Transform = player_q.single()[0];
+    const player_tex: rl.Texture2D = player_q.single()[1];
 
-    if (rl.isKeyDown(.j)) transform.y += VELOCITY;
-    if (rl.isKeyDown(.k)) transform.y -= VELOCITY;
-    if (rl.isKeyDown(.h)) transform.x -= VELOCITY;
-    if (rl.isKeyDown(.l)) transform.x += VELOCITY;
+    const map_grid: common.Grid =
+        (try w
+            .entity(map_q.single()[0].grid_entity)
+            .getComponents(&.{common.Grid}))[0];
+
+    if (rl.isKeyDown(.j)) {
+        if (try getDirectedBlock(
+            player_tex,
+            player_transform.*,
+            map_grid.cell_width,
+            map_grid.cell_height,
+            map_grid,
+            .down,
+        )) return;
+
+        player_transform.y += VELOCITY;
+    }
+    if (rl.isKeyDown(.k)) {
+        if (try getDirectedBlock(
+            player_tex,
+            player_transform.*,
+            map_grid.cell_width,
+            map_grid.cell_height,
+            map_grid,
+            .up,
+        ))
+            return;
+
+        player_transform.y -= VELOCITY;
+    }
+    if (rl.isKeyDown(.h)) {
+        if (try getDirectedBlock(
+            player_tex,
+            player_transform.*,
+            map_grid.cell_width,
+            map_grid.cell_height,
+            map_grid,
+            .left,
+        )) return;
+
+        player_transform.x -= VELOCITY;
+    }
+    if (rl.isKeyDown(.l)) {
+        if (try getDirectedBlock(
+            player_tex,
+            player_transform.*,
+            map_grid.cell_width,
+            map_grid.cell_height,
+            map_grid,
+            .right,
+        )) return;
+
+        player_transform.x += VELOCITY;
+    }
+}
+
+fn getDirectedBlock(
+    origin_texture: rl.Texture2D,
+    origin_transform: Transform,
+    block_width: i32,
+    block_height: i32,
+    map_grid: common.Grid,
+    direction: enum { up, down, left, right },
+) !bool {
+    const x = origin_transform.x;
+    const y = origin_transform.y;
+    const z = origin_transform.z;
+
+    const block: Transform = switch (direction) {
+        .up => .fromXYZ(x, y - block_height, z),
+        .down => .fromXYZ(x, y + block_height, z),
+        .left => .fromXYZ(x - block_width, y, z),
+        .right => .fromXYZ(x + block_width, y, z),
+    };
+
+    return isCollided(origin_texture, block, map_grid);
+}
+
+fn isCollided(
+    texture: rl.Texture2D,
+    transform: Transform,
+    map_grid: common.Grid,
+) !bool {
+    const idx_from_pixels = map_grid.getVirtualPositionFromPixels(
+        transform.x + @divTrunc(texture.width, 2), // center x
+        transform.y + @divTrunc(texture.width, 2), // center y
+    );
+    const idx = map_grid.getActualIndex(
+        idx_from_pixels.y,
+        idx_from_pixels.x,
+    ) catch return false;
+
+    return collision_block_arr[idx] == 1;
 }
 
 fn onWindowResize(
