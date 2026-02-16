996321eb764024e4116d857a8d012639b2c69b0f
LeviathanST <phamquochung170505@gmail.com>
2026-02-16 16:17:45 +0700

    player: separate system mod; add attack animation & `onAttack`

--- src/features/player/mod.zig
+++ src/features/player/mod.zig
@@ -1,32 +1,75 @@
+const std = @import("std");
 const eno = @import("eno");
 const common = eno.common;
 const rl = common.raylib;
 const scheds = common.schedules;
-const grid_collision = @import("extra_modules").grid_collision;
 
 const World = eno.ecs.World;
-const Query = eno.ecs.query.Query;
-const With = eno.ecs.query.With;
 const Transform = common.Transform;
 
-const Map = @import("../map/mod.zig").Map;
+const systems = @import("systems.zig");
 
-const Player = struct {};
+pub const MOVEMENT_VELOCITY = 5;
 
-const VELOCITY = 5;
+pub const Player = struct {
+    health: u32 = 100,
+};
+
+pub const SkillAnimationInfo = struct {
+    texture: rl.Texture,
+    total_frames: u8, // 0..255
+    current_frame: u8 = 0,
+    /// timestamp of the last rendering (ms)
+    last: i64 = 0,
+    /// duration rendering between frames (ms)
+    duration: u32,
+
+    pub fn init(
+        filename: [:0]const u8,
+        total_frames: u8,
+        duration: u32,
+    ) SkillAnimationInfo {
+        return .{
+            .texture = rl.Texture.init(filename) catch @panic("Loading slash texture failed"),
+            .total_frames = total_frames,
+            .current_frame = total_frames,
+            .duration = duration,
+        };
+    }
+
+    /// Start to record time of the frame rendering
+    pub fn start(self: *SkillAnimationInfo) void {
+        self.last = std.time.milliTimestamp();
+    }
+
+    /// return `true` if the elapsed time is greater than `duration`
+    pub fn tick(self: SkillAnimationInfo) bool {
+        return (std.time.milliTimestamp() - self.last) >= self.duration;
+    }
+
+    pub fn deinit(self: *const SkillAnimationInfo) void {
+        self.texture.unload();
+    }
+};
 
 pub fn build(w: *World) void {
     _ = w
+        .addResource(SkillAnimationInfo, .init(
+            "assets/animations/slash_sword/fire_slash.png",
+            6,
+            30,
+        ))
         .addSystem(.system, scheds.startup, spawn)
         .addSystems(
-        .system,
-        scheds.update,
-        &.{
-            movement,
-            updateCam,
-            onWindowResize,
-        },
-    );
+            .system,
+            scheds.update,
+            &.{
+                systems.movement,
+                systems.updateCam,
+                systems.onWindowResize,
+                systems.onAttack,
+            },
+        ).addSystem(.render, eno.render.schedules.process_render, systems.drawSlashAnimation);
 }
 
 fn spawn(w: *World) !void {
@@ -45,91 +88,4 @@ fn spawn(w: *World) !void {
         try common.Texture2D.fromImage(map_img),
         Transform.fromXYZ(50, 50, 1),
     });
-}
-
-fn updateCam(
-    player_q: Query(&.{
-        *rl.Camera2D,
-        Transform,
-        With(&.{Player}),
-    }),
-) !void {
-    const cam, const transform = player_q.single();
-    cam.target = .init(@floatFromInt(transform.x), @floatFromInt(transform.y));
-
-    if (rl.isKeyPressed(.equal) and rl.isKeyDown(.left_control))
-        cam.zoom += 0.2;
-    if (rl.isKeyPressed(.minus) and rl.isKeyDown(.left_control))
-        cam.zoom -= 0.2;
-}
-
-fn movement(
-    w: *World,
-    player_q: Query(&.{ *Transform, rl.Texture2D, With(&.{Player}) }),
-    map_q: Query(&.{ common.InGrid, With(&.{Map}) }),
-) !void {
-    const player_transform: *Transform = player_q.single()[0];
-    const player_tex: rl.Texture2D = player_q.single()[1];
-
-    const map_grid: common.Grid =
-        (try w
-            .entity(map_q.single()[0].grid_entity)
-            .getComponents(&.{common.Grid}))[0];
-
-    if (rl.isKeyDown(.j)) {
-        if (try grid_collision.getDirectedBlock(
-            player_tex,
-            player_transform.*,
-            map_grid,
-            .down,
-        )) return;
-
-        player_transform.y += VELOCITY;
-    }
-    if (rl.isKeyDown(.k)) {
-        if (try grid_collision.getDirectedBlock(
-            player_tex,
-            player_transform.*,
-            map_grid,
-            .up,
-        ))
-            return;
-
-        player_transform.y -= VELOCITY;
-    }
-    if (rl.isKeyDown(.h)) {
-        if (try grid_collision.getDirectedBlock(
-            player_tex,
-            player_transform.*,
-            map_grid,
-            .left,
-        )) return;
-
-        player_transform.x -= VELOCITY;
-    }
-    if (rl.isKeyDown(.l)) {
-        if (try grid_collision.getDirectedBlock(
-            player_tex,
-            player_transform.*,
-            map_grid,
-            .right,
-        )) return;
-
-        player_transform.x += VELOCITY;
-    }
-}
-
-fn onWindowResize(
-    player_q: Query(&.{
-        *rl.Camera2D,
-        With(&.{Player}),
-    }),
-) !void {
-    if (rl.isWindowResized()) {
-        const cam = player_q.single()[0];
-        cam.offset = .init(
-            @floatFromInt(@divTrunc(rl.getScreenWidth(), 2)),
-            @floatFromInt(@divTrunc(rl.getScreenHeight(), 2)),
-        );
-    }
 }
---
+++ src/features/player/systems.zig
@@ -0,0 +1,198 @@
+const std = @import("std");
+const eno = @import("eno");
+const common = eno.common;
+const rl = common.raylib;
+const grid_collision = @import("extra_modules").grid_collision;
+const health_bar = @import("extra_modules").health_bar;
+const mod = @import("mod.zig");
+
+const World = eno.ecs.World;
+const Query = eno.ecs.query.Query;
+const Resource = eno.ecs.query.Resource;
+const With = eno.ecs.query.With;
+const Transform = common.Transform;
+
+const Map = @import("../map/mod.zig").Map;
+const Monster = @import("../monster/mod.zig").Monster;
+
+const VELOCITY = mod.MOVEMENT_VELOCITY;
+const Player = mod.Player;
+const PlayerSkillAnimationInfo = mod.SkillAnimationInfo;
+
+pub fn updateCam(
+    player_q: Query(&.{
+        *rl.Camera2D,
+        Transform,
+        With(&.{Player}),
+    }),
+) !void {
+    const cam, const transform = player_q.single();
+    cam.target = .init(@floatFromInt(transform.x), @floatFromInt(transform.y));
+
+    if (rl.isKeyPressed(.equal) and rl.isKeyDown(.left_control))
+        cam.zoom += 0.2;
+    if (rl.isKeyPressed(.minus) and rl.isKeyDown(.left_control))
+        cam.zoom -= 0.2;
+}
+
+pub fn movement(
+    w: *World,
+    player_q: Query(&.{ *Transform, rl.Texture2D, With(&.{Player}) }),
+    map_q: Query(&.{ common.InGrid, With(&.{Map}) }),
+) !void {
+    const player_transform: *Transform = player_q.single()[0];
+    const player_tex: rl.Texture2D = player_q.single()[1];
+
+    const map_grid: common.Grid =
+        (try w
+            .entity(map_q.single()[0].grid_entity)
+            .getComponents(&.{common.Grid}))[0];
+
+    if (rl.isKeyDown(.s)) {
+        if (try grid_collision.getDirectedBlock(
+            player_tex,
+            player_transform.*,
+            map_grid,
+            .down,
+        )) return;
+
+        player_transform.y += VELOCITY;
+    }
+    if (rl.isKeyDown(.w)) {
+        if (try grid_collision.getDirectedBlock(
+            player_tex,
+            player_transform.*,
+            map_grid,
+            .up,
+        )) return;
+
+        player_transform.y -= VELOCITY;
+    }
+    if (rl.isKeyDown(.a)) {
+        if (try grid_collision.getDirectedBlock(
+            player_tex,
+            player_transform.*,
+            map_grid,
+            .left,
+        )) return;
+
+        player_transform.x -= VELOCITY;
+    }
+    if (rl.isKeyDown(.d)) {
+        if (try grid_collision.getDirectedBlock(
+            player_tex,
+            player_transform.*,
+            map_grid,
+            .right,
+        )) return;
+
+        player_transform.x += VELOCITY;
+    }
+}
+
+pub fn onAttack(
+    w: *World,
+    skill_anim_res: Resource(*PlayerSkillAnimationInfo),
+    monster_q: Query(&.{
+        rl.Texture,
+        Transform,
+        health_bar.HealthBarTarget,
+        With(&.{Monster}),
+    }),
+    player_q: Query(&.{
+        rl.Texture,
+        Transform,
+        With(&.{Player}),
+    }),
+) !void {
+    if (rl.isKeyPressed(.j)) {
+        const p_tex, const p_transform = player_q.single();
+        const skill_anim = skill_anim_res.result;
+        skill_anim.current_frame = 0;
+
+        const p_center_x: f32 = @floatFromInt(p_transform.x + @divTrunc(p_tex.width, 2));
+        const p_center_y: f32 = @floatFromInt(p_transform.y + @divTrunc(p_tex.height, 2));
+
+        const monster_half_width = @divTrunc(monster_q.single()[0].width, 2);
+        const monster_half_height = @divTrunc(monster_q.single()[0].height, 2);
+
+        // TODO: enhance by spatial queries
+        for (monster_q.many()) |query| {
+            _, const m_transform, const hb = query;
+            const health: *health_bar.HealthBar =
+                (try w
+                    .entity(hb.hb_id)
+                    .getComponents(&.{*health_bar.HealthBar}))[0];
+
+            if (rl.Vector2.distance(
+                .init(
+                    @floatFromInt(m_transform.x + monster_half_width),
+                    @floatFromInt(m_transform.y + monster_half_height),
+                ),
+                .init(p_center_x, p_center_y),
+            ) <= 25) {
+                health.curr_value -= 10;
+            }
+        }
+    }
+}
+
+pub fn drawSlashAnimation(
+    skill_anim_res: Resource(*PlayerSkillAnimationInfo),
+    player_q: Query(&.{ Transform, With(&.{Player}) }),
+) !void {
+    const skill_anim = skill_anim_res.result;
+    // Skip if at the final frame
+    if (skill_anim.current_frame == skill_anim.total_frames)
+        return;
+
+    const player_transform = player_q.single()[0];
+    const frame_width: f32 = @floatFromInt(@divTrunc(skill_anim.texture.width, 6));
+    const frame_height: f32 = @floatFromInt(@divTrunc(skill_anim.texture.height, 5));
+    const scale_factor = 0.7;
+
+    var origin_frame_rec: rl.Rectangle = .{
+        .x = 0,
+        .y = frame_height * 1,
+        .width = frame_width,
+        .height = frame_height,
+    };
+    const scaled_frame_rec: rl.Rectangle = .{
+        .x = 0,
+        .y = 0,
+        .width = frame_width * scale_factor,
+        .height = frame_height * scale_factor,
+    };
+    origin_frame_rec.x = @as(f32, @floatFromInt(skill_anim.current_frame)) * frame_width;
+
+    skill_anim.texture.drawPro(
+        origin_frame_rec,
+        scaled_frame_rec,
+        .init(
+            @floatFromInt(-player_transform.x + 15),
+            @floatFromInt(-player_transform.y + 25),
+        ),
+        0,
+        .white,
+    );
+
+    if (skill_anim.tick()) {
+        skill_anim.start();
+        skill_anim.current_frame += 1;
+    }
+}
+
+pub fn onWindowResize(
+    player_q: Query(&.{
+        *rl.Camera2D,
+        With(&.{Player}),
+    }),
+) !void {
+    if (rl.isWindowResized()) {
+        const cam = player_q.single()[0];
+        cam.offset = .init(
+            @floatFromInt(@divTrunc(rl.getScreenWidth(), 2)),
+            @floatFromInt(@divTrunc(rl.getScreenHeight(), 2)),
+        );
+    }
+}
