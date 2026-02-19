f85a02544134cddced902b10bf65b456897a0e27
LeviathanST <phamquochung170505@gmail.com>
2026-02-19 22:10:39 +0700

    gui: add `hit`

--- src/gui/mod.zig
+++ src/gui/mod.zig
@@ -5,6 +5,12 @@ const World = ecs.World;
 
 const player_health_bar = @import("player_health_bar/mod.zig");
 
+const hit = @import("hit/mod.zig");
+pub const Hit = hit.Hit;
+
 pub fn build(w: *World) void {
-    _ = w.addModules(&.{player_health_bar});
+    _ = w.addModules(&.{
+        player_health_bar,
+        hit,
+    });
 }
